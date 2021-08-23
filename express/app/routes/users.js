1f979ec64b4cbe806475cb8623c4a56f1b3af9e1
spiseb <sebastien.spirito@student.intecbrussel.be>
2021-08-23 12:38:09 +0200

    'user table' update

--- express/app/routes/users.js
+++ express/app/routes/users.js
@@ -1,9 +1,18 @@
 var express = require('express');
 var router = express.Router();
+const fetch = require('node-fetch')
+
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get('/', function(req, res) {
+  fetch('https://jsonplaceholder.typicode.com/users')
+  .then(x => x.json())
+  .then(data => { 
+    console.log(data)
+    res.render('users', {users: data});
+  })
+
+ 
 });
 
 module.exports = router;
